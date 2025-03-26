174f09a935021b510455b33f2f264695a33e3fb2
Yurii Ostapchuk <yntedero@gmail.com>
2025-03-26 01:20:56 +0100

    Refactor project structure by moving HTML, CSS, and JS files to the src directory and removing old files

--- src/scripts.js
+++ src/scripts.js
@@ -1,12 +1,11 @@
 console.log("Hello, world!");
 
-
 function greet(name) {
-    return `Hello, ${name}!`;
+  return `Hello, ${name}!`;
 }
 
 console.log(greet("User"));
 
 document.addEventListener("DOMContentLoaded", () => {
-    console.log("Document is fully loaded and parsed.");
+  console.log("Document is fully loaded and parsed.");
 });
